91d0f5c38256ca0b619463f1460ca593183b82b7
Bach <bach.ly@invenco.net>
2024-05-17 15:25:33 +1000

    First change

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,17 @@ import { Button } from "components/Button/Button"
 import { LP_GRID_ITEMS } from "lp-items"
 
 export const metadata: Metadata = {
-  title: "Next.js Enterprise Boilerplate",
+  title: "Here | Link-in-bio page with built-in sales funnels",
   twitter: {
     card: "summary_large_image",
   },
   openGraph: {
-    url: "https://next-enterprise.vercel.app/",
+    url: "https://here.li",
     images: [
       {
         width: 1200,
         height: 630,
-        url: "https://raw.githubusercontent.com/Blazity/next-enterprise/main/.github/assets/project-logo.png",
+        url: "https://raw.githubusercontent.com/Blazity/next-enterprise/main/.github/assets/project-logo.png", // TODO
       },
     ],
   },
@@ -25,28 +25,26 @@ export default function Web() {
       <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
+            <div className="text-center font-bold mb-12 text-xl md:text-2xl xl:text-3xl">Here.li</div>
             <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
-              Next.js Enterprise Boilerplate
+              One-page funnels with a memorable link
             </h1>
             <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
-              Jumpstart your enterprise project with our feature-packed, high-performance Next.js boilerplate!
-              Experience rapid UI development, AI-powered code reviews, and an extensive suite of tools for a smooth and
-              enjoyable development process.
+              Create 1-page funnels with Here.li, featuring a memorable link that drives conversions and simplifies your online marketing strategy. Boost engagement and sales effortlessly with this easy-to-use solution.
             </p>
-            <Button href="https://github.com/Blazity/next-enterprise" className="mr-3">
-              Get started
-            </Button>
-            <Button
-              href="https://vercel.com/new/git/external?repository-url=https://github.com/Blazity/next-enterprise"
-              intent="secondary"
-            >
-              Deploy Now
+            <Button href="mailto:bach@bach.au" className="mr-3">
+              Claim your link
             </Button>
           </div>
         </div>
       </section>
       <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-16 lg:px-6">
+
+          <h1 className="mb-12 max-w-2xl mx-auto text-2xl font-extrabold leading-none tracking-tight dark:text-white md:text-3xl xl:text-4xl">
+            Technologies used behind every link
+          </h1>
+
           <div className="justify-center space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-3">
             {LP_GRID_ITEMS.map((singleItem) => (
               <div key={singleItem.title} className="flex flex-col items-center justify-center text-center">
